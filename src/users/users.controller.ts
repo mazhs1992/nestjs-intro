f580810c87b71425b427166e87970ac050cb64d1
mazhs1992 <mazhs1992@hotmail.com>
2024-11-04 23:19:44 +0200

    get user - new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Patch,
+  Put,
+  Delete,
+  Headers,
+  Ip,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id/:optional?')
+  //   public getUsers(@Param() params: any, @Query() query: any) {
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
+    return `You send a get resuest to /users `;
+  }
+
+  @Post()
+  public createUsers(
+    @Body() body: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(body);
+    console.log(ip);
+    console.log(headers);
+
+    return 'You send a post resuest to /users';
+  }
+
+  //   @Post()
+  //   public createUsers(@Req() req: Request) {
+  //     console.log(req.body);
+  //     return 'You send a post resuest to /users';
+  //   }
+}
