8fad869b8f203e8d4bea2565cd86e0617a86bc1e
mazhs1992 <mazhs1992@hotmail.com>
2024-11-10 22:54:31 +0200

    added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,29 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     PostsModule,
     AuthModule,
-    // check TYPEORM for configuration
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      entities: [],
-      synchronize: true,
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'nestjs-blog',
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        host: 'localhost',
+        port: 5432,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+      }),
     }),
   ],
   controllers: [AppController],
