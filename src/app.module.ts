c4fb4201c2ef66f620eb6da07b1a1da01ee23799
mazhs1992 <mazhs1992@hotmail.com>
2024-11-10 22:52:11 +0200

    connecting with postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    // check TYPEORM for configuration
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'nestjs-blog',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
