3b4e741a5590231bd36b521f35124d04becc65c1
mazhs1992 <mazhs1992@hotmail.com>
2024-11-19 09:17:08 +0200

    configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
-
+import { appConfig } from './config/app.config';
 const NODE_ENV = process.env.NODE_ENV;
 
 @Module({
@@ -16,6 +16,7 @@ const NODE_ENV = process.env.NODE_ENV;
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !NODE_ENV ? '.env' : `.env.${NODE_ENV}`,
+      load: [appConfig],
     }),
     UsersModule,
     PostsModule,
@@ -26,13 +27,13 @@ const NODE_ENV = process.env.NODE_ENV;
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         // entities: [User],
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        synchronize: configService.get('database.synchronize'),
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
       }),
     }),
     TagsModule,
---
+++ src/config/app.config.ts
@@ -0,0 +1,12 @@
+export const appConfig = () => ({
+  environment: process.env.NODE_ENV || 'production',
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    synchronize: process.env.DATABASE_SYNC === 'true',
+    autoLoadEntities: process.env.DATABASE_AUTO_LOAD === 'true',
+  },
+});
